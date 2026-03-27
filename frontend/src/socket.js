369397ff9abff77f9571ce304f10e14d64eee35b
Musn0o <askarmoustafa@yahoo.com>
2026-03-27 15:01:28 +0200

    Update socket URL to live production backend

--- frontend/src/socket.js
+++ frontend/src/socket.js
@@ -1,6 +1,6 @@
 import { io } from 'socket.io-client';
 
-const URL = 'http://localhost:3001';
+const URL = 'https://denk-fix-online.onrender.com';
 export const socket = io(URL, {
   autoConnect: false
 });
